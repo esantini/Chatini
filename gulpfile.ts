20fefef4417fffe77406907b24b735b854c7e853
Esteban Santini <esantinie@gmail.com>
2017-04-20 12:24:07 -0700

    Removing "watcher" task from the defaults. I'll just execute "gulp" each time I need to. For now.

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,5 +1,5 @@
 /*
-    Generated from typescript file
+	Generated from typescript file
 */
 
 import * as gulp from 'gulp';
@@ -9,27 +9,27 @@ var wachAll: Array<{ name:string, src: string | string[], dest: string }> = [];
 var wacha: { name: string, src: string | string[], dest: string };
 //
 wacha = {
-    name: "myTypeScripts" ,
-    src: 'src/**/*.ts',
-    dest: 'build/'
+	name: "myTypeScripts" ,
+	src: 'src/**/*.ts',
+	dest: 'build/'
 };
 wachAll.push(wacha);
 (function(name: string, src: string | string[], dest: string): void {
-    var tsProject = tsc.createProject('tsconfig.json');
-    gulp.task( name , function() {
-        gulp.src(src)
-            .pipe(tsProject())
-            .pipe(gulp.dest( dest ));
-    });
+	var tsProject = tsc.createProject('tsconfig.json');
+	gulp.task( name , function() {
+		gulp.src(src)
+			.pipe(tsProject())
+			.pipe(gulp.dest( dest ));
+	});
 })(wacha.name, wacha.src, wacha.dest);
 
 //
 //
 
 wacha = {
-    name: "allButTs",
-    src: ['src/**/*', '!src/**/*.ts'],
-    dest: 'build/'
+	name: "allButTs",
+	src: ['src/**/*', '!src/**/*.ts'],
+	dest: 'build/'
 };
 wachAll.push(wacha);
 (function(name: string, src: string | string[], dest: string) {
@@ -42,57 +42,57 @@ wachAll.push(wacha);
 })(wacha.name, wacha.src, wacha.dest);
 //
 wacha = {
-    name: "gulpfile",
-    src: 'gulpfile.ts',
-    dest: ''
+	name: "gulpfile",
+	src: 'gulpfile.ts',
+	dest: ''
 }
 wachAll.push(wacha);
 (function(name: string, src: string | string[], dest: string) {
 	
-    gulp.task(name, function() {
-        var tsProject = tsc.createProject('tsconfig.json');
-        gulp.src(src)
-            .pipe(tsProject())
-            .pipe(gulp.dest(dest));
-    });
+	gulp.task(name, function() {
+		var tsProject = tsc.createProject('tsconfig.json');
+		gulp.src(src)
+			.pipe(tsProject())
+			.pipe(gulp.dest(dest));
+	});
 
 })(wacha.name, wacha.src, wacha.dest);
 //
 //
 wacha = {
-    name: "tests",
-    src: 'tests/**/*.ts',
-    dest: 'tests'
+	name: "tests",
+	src: 'tests/**/*.ts',
+	dest: 'tests'
 }
 wachAll.push(wacha);
 (function(name: string, src: string | string[], dest: string) {
 	
-    gulp.task(name, function() {
-        var tsProject = tsc.createProject('tsconfig.json');
-        gulp.src(src)
-            .pipe(tsProject())
-            .pipe(gulp.dest(dest));
-    });
+	gulp.task(name, function() {
+		var tsProject = tsc.createProject('tsconfig.json');
+		gulp.src(src)
+			.pipe(tsProject())
+			.pipe(gulp.dest(dest));
+	});
 
 })(wacha.name, wacha.src, wacha.dest);
 // 
 // 
 gulp.task('watch_all', function() {
-    wachAll.forEach(wacha => {
-        // var src:string = typeof wacha.src === 'string' ?  wacha.src : wacha.src[0];
-        gulp.watch(wacha.src, [wacha.name]);
-    });
+	wachAll.forEach(wacha => {
+		// var src:string = typeof wacha.src === 'string' ?  wacha.src : wacha.src[0];
+		gulp.watch(wacha.src, [wacha.name]);
+	});
 });
 
 // set allTasks as an array of names, example: ['typescripts', 'jades', 'htmls']
 var allTasks = (function(){
-    var arr: string[] = [];
-    wachAll.forEach(task => {
-        console.log('Wacha', task.name);
-        arr.push(task.name);
-    });
-    return arr;
+	var arr: string[] = [];
+	wachAll.forEach(task => {
+		console.log('Wacha', task.name);
+		arr.push(task.name);
+	});
+	return arr;
 })();
-allTasks.push('watch_all');
+// allTasks.push('watch_all');
 
 gulp.task('default', allTasks );
