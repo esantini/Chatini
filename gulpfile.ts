7b55ad81350fd79d93c02f8f0a3b23859e832af3
Esteban Santini <esantinie@gmail.com>
2017-04-26 10:03:37 -0700

    minor corrections & variable naming improvements

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,11 +5,17 @@
 import * as gulp from 'gulp';
 import * as tsc from 'gulp-typescript';
 //
-var wachAll: Array<{ name:string, src: string | string[], dest: string }> = [];
-var wacha: { name: string, src: string | string[], dest: string };
+interface Wacha {
+	taskName: string,
+	src: string | string[],
+	dest: string
+}
+var wacha: Wacha;
+var wachAll: Array<Wacha> = [];
+
 //
 wacha = {
-	name: "myTypeScripts",
+	taskName: "myTypeScripts",
 	src: 'src/**/*.ts',
 	dest: 'build/'
 };
@@ -21,14 +27,14 @@ wachAll.push(wacha);
 			.pipe(tsProject())
 			.pipe(gulp.dest( dest ));
 	});
-})(wacha.name, wacha.src, wacha.dest);
+})(wacha.taskName, wacha.src, wacha.dest);
 
 //
 //
 
 wacha = {
-	name: "allButTs",
-	src: ['src/**/*', '!src/**/*.ts'],
+	taskName: "move_plain_files",
+	src: ['src/**/*', '!src/**/*.ts', '!src/**/*.less'],
 	dest: 'build/'
 };
 wachAll.push(wacha);
@@ -39,10 +45,10 @@ wachAll.push(wacha);
 			.pipe(gulp.dest(dest));
 	});
 
-})(wacha.name, wacha.src, wacha.dest);
+})(wacha.taskName, wacha.src, wacha.dest);
 //
 wacha = {
-	name: "gulpfile",
+	taskName: "gulpfile",
 	src: 'gulpfile.ts',
 	dest: ''
 };
@@ -56,11 +62,11 @@ wachAll.push(wacha);
 			.pipe(gulp.dest(dest));
 	});
 
-})(wacha.name, wacha.src, wacha.dest);
+})(wacha.taskName, wacha.src, wacha.dest);
 //
 //
 wacha = {
-	name: "tests",
+	taskName: "tests",
 	src: 'tests/**/*.ts',
 	dest: 'tests'
 };
@@ -74,13 +80,13 @@ wachAll.push(wacha);
 			.pipe(gulp.dest(dest));
 	});
 
-})(wacha.name, wacha.src, wacha.dest);
+})(wacha.taskName, wacha.src, wacha.dest);
 // 
 // 
 gulp.task('watch_all', function() {
 	wachAll.forEach(wacha => {
 		// var src:string = typeof wacha.src === 'string' ?  wacha.src : wacha.src[0];
-		gulp.watch(wacha.src, [wacha.name]);
+		gulp.watch(wacha.src, [wacha.taskName]);
 	});
 });
 
@@ -88,8 +94,8 @@ gulp.task('watch_all', function() {
 var allTasks = (function(){
 	var arr: string[] = [];
 	wachAll.forEach(task => {
-		console.log('Wacha', task.name);
-		arr.push(task.name);
+		console.log('Wacha', task.taskName);
+		arr.push(task.taskName);
 	});
 	return arr;
 })();
@@ -99,14 +105,17 @@ gulp.task('build', function() {
 	gulp.src('node_modules/angular/angular.min.js')
 		.pipe(gulp.dest('build/public/javascripts/angular'));
 
-	gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
-		.pipe(gulp.dest('build/public/javascripts'));
+	// gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
+	// 	.pipe(gulp.dest('build/public/javascripts'));
 
 	gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
 		.pipe(gulp.dest('build/public/stylesheets'));
 
 	gulp.src('node_modules/bootstrap/dist/fonts/*')
 		.pipe(gulp.dest('build/public/fonts'));
+
+	// gulp.src('node_modules/jquery/dist/jquery.min.js')
+	// 	.pipe(gulp.dest('build/public/javascripts'));
 });
 allTasks.push('build');
 
