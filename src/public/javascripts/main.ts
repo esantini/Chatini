84f694277757adebf4dc06bc7dff7199ec9e1745
Esteban Santini <esantinie@gmail.com>
2017-04-26 10:17:15 -0700

    Objects definitions.
    Chat categories: "group" and "user".
    
    Functionality to filter chats list by category.

--- src/public/javascripts/main.ts
+++ src/public/javascripts/main.ts
@@ -1,14 +1,57 @@
 
 var myApp = angular.module("myChatini", []);
 
-myApp.controller('myChatsCtrl', function($scope) {
+interface ChatType {
+	name: string,
+	category: string
+}
+interface MyChatsScope {
+	chatsList: Array<ChatType>,
+	chatCategory: string,
+	chatCategoryClick: Function
+}
+
+myApp.controller('myChatsCtrl', function($scope: MyChatsScope) {
+	
 	$scope.chatsList = [
+		{	name: 'chatGroup1',
+			category: 'group' },
 		{
-			name: 'chatGroup1'
-		},
+			name: 'chatGroup2',
+			category: 'group' },
 		{
-			name: 'chatGroup2'
+			name: 'chatPerson1',
+			category: 'user'
 		}
-	]
-})
+	];
+	$scope.chatCategory = 'all';
+	$scope.chatCategoryClick = function(type: string) {
+		if(type != 'all' && type != 'user' && type != 'group') {
+			return; // TODO return error.
+		}
+		$scope.chatCategory = type;
+
+		angular.element(document.querySelector('.chatcategory.active')).removeClass('active');
+		angular.element(document.querySelector('.chatcategory.'+type)).addClass('active');
+
+	}
+});
+
+myApp.filter('categoryFilter', function() {
+	return function(elements: Array<ChatType>, currentCategory: string) {
+		
+		if(currentCategory == 'all') {
+			return elements;
+		}
+		
+		var filtered: Array<ChatType> = [];
+		for (var i = 0; i < elements.length; i++) {
+			if(elements[i].category == currentCategory) {
+				filtered.push(elements[i]);
+			}
+		}
+		return filtered;
+		
+	}
+});
 
