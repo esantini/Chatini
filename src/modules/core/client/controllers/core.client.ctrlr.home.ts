b1fa43488a8f9d2e57cf21b7cf8b3999535e7f29
Esteban Santini <esantinie@gmail.com>
2017-05-16 17:41:01 -0700

    An approach at showing the home's view differently when logged in or logged out.

--- src/modules/core/client/controllers/core.client.ctrlr.home.ts
+++ src/modules/core/client/controllers/core.client.ctrlr.home.ts
@@ -1,8 +1,16 @@
 (function() {
 
-	angular.module('myChatini')
-		.controller('homeCtrl', function() {
-			console.log('Home Controller Initialized');
-		});
+	angular
+		.module('myChatini')
+		.controller('homeCtrl', homeCtrl);
 
+	homeCtrl.$inject = ['authentication'];
+	function homeCtrl(authentication: any) {
+		console.log('Home Controller Initialized');
+
+		var homeScope = this;
+
+		homeScope.isLoggedIn = authentication.isLoggedIn();
+
+	}
 })();
