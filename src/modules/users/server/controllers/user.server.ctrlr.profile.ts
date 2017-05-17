ef6f59c7070e59b26fc843427b1e50001e0a8f4d
Esteban Santini <esantinie@gmail.com>
2017-05-16 17:32:00 -0700

    Because Tabs is better than Spaces ;)

--- src/modules/users/server/controllers/user.server.ctrlr.profile.ts
+++ src/modules/users/server/controllers/user.server.ctrlr.profile.ts
@@ -4,16 +4,16 @@ var User = mongoose.model('User');
 
 export const profileRead = function(req: any, res: express.Response) {
 
-  if (!req.payload._id) {
-    res.status(401).json({
-      "message" : "UnauthorizedError: private profile"
-    });
-  } else {
-    User
-      .findById(req.payload._id)
-      .exec(function(err, user) {
-        res.status(200).json(user);
-      });
-  }
+	if (!req.payload._id) {
+		res.status(401).json({
+			"message" : "UnauthorizedError: private profile"
+		});
+	} else {
+		User
+			.findById(req.payload._id)
+			.exec(function(err, user) {
+				res.status(200).json(user);
+			});
+	}
 
 };
