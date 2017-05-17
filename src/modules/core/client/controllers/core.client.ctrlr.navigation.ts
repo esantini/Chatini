24364fee8640e7363410dc246217735ba1a7cb4f
Esteban Santini <esantinie@gmail.com>
2017-05-16 17:29:18 -0700

    Navigation Bar Directive corrected & placed in index.ejs

--- src/modules/core/client/controllers/core.client.ctrlr.navigation.ts
+++ src/modules/core/client/controllers/core.client.ctrlr.navigation.ts
@@ -5,21 +5,26 @@
 		.controller('navigationCtrl', navigationCtrl)
 		.directive('navigation', navigation);
 
-	navigationCtrl.$inject = ['$location','authentication'];
-	function navigationCtrl($location:any, authentication:any) {
+	navigationCtrl.$inject = ['$location','authentication', '$route'];
+	function navigationCtrl($location:any, authentication:any, $route: angular.route.IRouteService) {
 		var vm = this;
 
 		vm.isLoggedIn = authentication.isLoggedIn();
-
 		vm.currentUser = authentication.currentUser();
 
+		vm.logout = function() {
+			authentication.logout();
+			vm.isLoggedIn = authentication.isLoggedIn();
+			vm.currentUser = authentication.currentUser();
+			$route.reload();
+		}
 	}
 
 
 	function navigation () {
 		return {
 			restrict: 'EA',
-			templateUrl: '/common/directives/navigation/navigation.template.html',
+			templateUrl: '/views/core.navigation.html',
 			controller: 'navigationCtrl as navvm'
 		};
 	}
