35beeca442851adbb78a5aefb44f1cc40308a216
Esteban Santini <esantinie@gmail.com>
2017-05-25 16:36:33 -0700

    Listen to $rootScope's "log" event to update the home's view when the user logs in.

--- src/modules/core/client/controllers/core.client.ctrlr.home.ts
+++ src/modules/core/client/controllers/core.client.ctrlr.home.ts
@@ -4,13 +4,17 @@
 		.module('myChatini')
 		.controller('homeCtrl', homeCtrl);
 
-	homeCtrl.$inject = ['authentication'];
-	function homeCtrl(authentication: any) {
+	homeCtrl.$inject = ['authentication', '$rootScope'];
+	function homeCtrl(authentication: any, $rootScope: angular.IRootScopeService) {
 		console.log('Home Controller Initialized');
 
 		var homeScope = this;
 
 		homeScope.isLoggedIn = authentication.isLoggedIn();
+
+		$rootScope.$on("log", function() {
+			homeScope.isLoggedIn = authentication.isLoggedIn();
+		});
 
 	}
 })();
