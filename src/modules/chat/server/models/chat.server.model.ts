e2e9104ba49f41da139256ba6513e1f24ac7b266
Esteban Santini <esantinie@gmail.com>
2017-05-26 12:50:08 -0700

    1st draft of the Conversation/Message Schemas

---
+++ src/modules/chat/server/models/chat.server.model.ts
@@ -0,0 +1,48 @@
+
+import { Document, Schema, Model, model } from 'mongoose';
+import { MyUser } from "../../../users/server/models/user.server.model";
+
+export interface Conversation extends Document {
+	category: string,
+	members: string[], // The user IDs (email)
+	messages: Message[]
+}
+
+export interface Message extends Document {
+	from: string, // 
+	message: string | Drawing,
+	date: Date
+}
+
+interface Drawing {}
+
+var MessageSchema: Schema = new Schema({
+	from: { type: Number, ref: 'User' },
+	message: String,
+	date: Date
+});
+
+var ConversationSchema: Schema = new Schema({
+	category: {
+		type: String,
+		trim: true,
+		default: 'user',
+		required: true
+	},
+	created: Date,
+	creator: { type: Number, ref: 'User' },
+	members: [{ type: Number, ref: 'User' }],
+	messages: {
+		type: [MessageSchema],
+		default: [],
+		required: true
+	}
+});
+
+export
+	const Conversation: Model<Conversation> = 
+	model<Conversation>("Conversation", ConversationSchema);
+
+export 
+	const Message: Model<Message> =
+	model<Message>("Message", MessageSchema);
