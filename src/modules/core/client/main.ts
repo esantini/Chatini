1f63d3a3d5d135a46739e5306735f902488e4a9c
Esteban Santini <esantinie@gmail.com>
2017-05-31 15:04:46 -0700

    Using angular's configurations to set material's theme color palletes.

--- src/modules/core/client/main.ts
+++ src/modules/core/client/main.ts
@@ -2,4 +2,11 @@
  * This file is the 1st one to execute, 
  *   it is placed at the top of the concatenated bundle.
  */
-angular.module("myChatini", ['ui.router', 'ngMaterial']);
+angular.module("myChatini", ['ui.router', 'ngMaterial'])
+	.config(['$mdThemingProvider',
+		function($mdThemingProvider: angular.material.IThemingProvider) {
+			$mdThemingProvider.theme('default')
+				.primaryPalette('light-green')
+				.accentPalette('brown');
+		}
+	]);
