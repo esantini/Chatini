1e82f3e5411c8701bca818eca4ff365f46e47539
Esteban Santini <esantinie@gmail.com>
2017-06-02 09:50:03 -0700

    Functionality to add a friend with autocomplete from angular material.

--- src/modules/chat/client/controllers/chat.client.ctrlr.add_friend.ts
+++ src/modules/chat/client/controllers/chat.client.ctrlr.add_friend.ts
@@ -1,24 +1,72 @@
 (function() {
 
-	angular.module('myChatini').controller('AddFriendController', 
-	function ($scope: angular.IScope, $mdDialog: angular.material.IDialogService) {
-		/* TODO:
-			What's better?
-				ctrlr: $scope.items = [1,2,3];
-				view: <div ng-repeat="item in items">
-			or?
-				ctrlr:	var myScope = this;
-						myScope.items = [1,2,3];
-				view: <div ng-repeat="item in ctrlr.items"
-		*/
-		
-		$scope.hide = function() { $mdDialog.hide() };
-		$scope.cancel = function() {console.log('cancel'); $mdDialog.cancel() };
-		$scope.submit = function(data: any) {
-			console.log('Submit form with data:', data);
+angular.module('myChatini').controller('AddFriendController', 
+function (
+		$scope: angular.IScope,
+		$mdDialog: angular.material.IDialogService,
+		$timeout: angular.ITimeoutService,
+		$q: angular.IQService,
+		userData: any) {
+	/* TODO:
+		What's better?
+			ctrlr: $scope.items = [1,2,3];
+			view: <div ng-repeat="item in items">
+		or?
+			ctrlr:	var myScope = this;
+					myScope.items = [1,2,3];
+			view: <div ng-repeat="item in ctrlr.items"
+	*/
+
+	$scope.submit = function(data: any) {
+		if(data) {
+			userData.friendRequest(data);
 			$mdDialog.hide();
 		}
-		$scope.items = [1, 2, 3];
-	});
+	}
+	$scope.items = ['ei', 'bi', 'si'];
+
+	// list of `state` value/display objects
+	$scope.selectedItem  = null;
+	$scope.searchText    = null;
+	$scope.querySearch   = querySearch;
+
+
+	// ******************************
+	// Internal methods
+	// ******************************
+
+	/**
+	 * Search for states... use $timeout to simulate
+	 * remote dataservice call.
+	 */
+	var deferred: angular.IDeferred<any>;
+	var timeoutId: NodeJS.Timer;
+	function querySearch (query: string) {
+		deferred = $q.defer();
+		clearTimeout(timeoutId);
+		timeoutId = setTimeout(function() {
+			userData.getUsersList(query).then(
+				function(data: any) {
+					deferred.resolve( data.data );
+				},
+				function(err: Error){ deferred.reject(); throw err; }
+			);
+
+		}, 500);
+		return deferred.promise;
+	}
+
+	/**
+	 * Create filter function for a query string
+	 */
+	// function createFilterFor(query: string) {
+	// 	var lowercaseQuery = angular.lowercase(query);
+
+	// 	return function filterFn(state: any) {
+	// 		return (state.value.indexOf(lowercaseQuery) === 0);
+	// 	};
+
+	// }
+});
 
 })();
