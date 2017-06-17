1e869fdba76811b101a488bc4f069865a65dcced
Esteban Santini <esantinie@gmail.com>
2017-06-16 17:43:28 -0700

    Profile controller can change the user's preferred language

--- src/modules/users/client/controllers/user.client.ctrlr.profile.ts
+++ src/modules/users/client/controllers/user.client.ctrlr.profile.ts
@@ -4,8 +4,12 @@ angular
 	.module('myChatini')
 	.controller('profileCtrl', profileCtrl);
 
-profileCtrl.$inject = ['$location', 'userData'];
-function profileCtrl($location: angular.ILocationService, userData: any) {
+profileCtrl.$inject = ['$location', 'userData', '$cookies', '$translate'];
+function profileCtrl(
+		$location: angular.ILocationService,
+		userData: any,
+		$cookies: angular.cookies.ICookiesService,
+		$translate: angular.translate.ITranslateService) {
 
 	var vm = this;
 
@@ -15,11 +19,28 @@ function profileCtrl($location: angular.ILocationService, userData: any) {
 		.then(
 			function(data: any) {
 				vm.user = data.data;
+				vm.currLanguage = vm.user.language;
 			},
 			function (e: string) {
 				console.error("My Profile Controller Error: ", e);
 			}
 		);
+
+	vm.languages = [
+		{	id: 'en',
+			val: 'English' },
+		{	id: 'es',
+			val: 'Español'},
+		{	id: 'de',
+			val: 'Deutch'}];
+	
+	vm.changeLang = function() {
+		userData.changeLang(vm.user.language).then(function(response: any) {
+			$cookies.put('language', vm.user.language);
+			$translate.use(vm.user.language);
+			location.reload();
+		});
+	}
 }
 
 })();
